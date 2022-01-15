9e34aa2bfed9e77603d9dc334da5906a70c315cb
Brandon <pacewicbrandon@gmail.com>
2022-01-15 15:32:58 -0800

    added user checks

--- _ignore_python_extention.py
+++ _ignore_python_extention.py
@@ -2,8 +2,8 @@ ALPH = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def main():
     with open('.gitignore', 'w') as file:
-        file.write('.gitignore')
-        file.writelines([x for x in ALPH])
+        file.write('.gitignore\n')
+        file.writelines([f'{x}.cc\n' for x in ALPH])
 
 if __name__ == '__main__':
     main()
--- ignore.sh
+++ ignore.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
+#!/bin/env bash
+
+set e
+
+SCRIPT_DIR="$(dirname "$0")"
+TARGET_FILE="$SCRIPT_DIR/.gitignore"
+
+if [[ -e $TARGET_FILE ]] 
+then
+    read -r -p "A .gitignore file already exists, are you sure you want to proceed? [N|y]: "
+    if ! [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Aborting"
+        exit 1
+    fi
+fi
 
 touch .gitignore
 python3 _ignore_python_extention.py
